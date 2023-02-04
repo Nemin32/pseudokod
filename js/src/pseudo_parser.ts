72801db8d2724a17652c203fec43e986aaf74e0e
Nemin32 <pergerlori@gmail.com>
2023-02-04 19:12:28 +0100

    Added variable, assignment, if (WIP) parsing.

--- js/src/pseudo_parser.ts
+++ js/src/pseudo_parser.ts
@@ -1,5 +1,23 @@
 import { Parser } from "./parser/parser.ts";
-import { Atom, Expression, make_atom, make_binop } from "./pseudo_types.ts";
+import {
+  AssignmentStatement,
+  Atom,
+  Block,
+  Expression,
+  IfStatement,
+  make_assignment,
+  make_atom,
+  make_binop,
+  make_if,
+  make_print,
+  make_variable,
+  PrintStatement,
+  Statement,
+  Variable,
+} from "./pseudo_types.ts";
+
+const OWS = Parser.char(" ").many();
+const WS = Parser.char(" ").many1();
 
 /* Atom */
 
@@ -21,21 +39,55 @@ const parseBool = Parser.or(
 ).expect("<bool>");
 
 const parseSingleAtom = parseNumber.or(parseString).or(parseBool).expect("<atom>");
-const parseArrayComprehension = parseSingleAtom.sepBy(Parser.char(",")).parens().expect("<atom list>");
+const parseArrayComprehension = parseSingleAtom
+  .sepBy(Parser.char(",").bind((_) => OWS))
+  .parens()
+  .expect("<atom list>");
 
 const parseAtom: Parser<Atom> = parseArrayComprehension.or(parseSingleAtom).bindResult(make_atom).expect("<atom>");
 
+/* Variable */
+
+const parseVariable: Parser<Variable> = Parser.letter.bind((l) => Parser.alphanumeric.many().bindResult((rest) => make_variable(l + rest.join(""))));
+
+/* Value */
+
+const parseValue = parseVariable.or(parseAtom);
+
 /* Binary Ops */
 
-const parseAddOp: Parser<string> = Parser.char("+").or(Parser.char("-"));
-const parseMulOp = Parser.char("*").or(Parser.char("/")).or(Parser.string("mod"));
+const parseAddOp: Parser<string> = Parser.char("+").or(Parser.char("-")).bracket(OWS, OWS);
+const parseMulOp = Parser.char("*").or(Parser.char("/")).or(Parser.string("mod")).bracket(OWS, OWS);
 
-const parseBinExp: Parser<Expression> = Parser.of(() => parseBinTerm).bindChain(parseAddOp, ({ f, a, b }) => make_binop({ op: f, exp1: a, exp2: b }));
-const parseBinTerm: Parser<Expression> = Parser.of(() => parseBinFactor).bindChain(parseMulOp, ({ f, a, b }) => make_binop({ op: f, exp1: a, exp2: b }));
-const parseBinFactor: Parser<Expression> = Parser.or(parseAtom, Parser.of(() => parseBinExp).parens());
+const parseBinExp: Parser<Expression> = Parser.of(() => parseBinTerm).bindChain(parseAddOp, ({ f, a, b }) => make_binop(f, a, b));
+const parseBinTerm: Parser<Expression> = Parser.of(() => parseBinFactor).bindChain(parseMulOp, ({ f, a, b }) => make_binop(f, a, b));
+const parseBinFactor: Parser<Expression> = Parser.or(parseValue, Parser.of(() => parseBinExp).parens());
 
 /* Expression */
 
 const parseExpression = parseBinExp;
 
-console.log(parseExpression.run("(5+5)+5*3").unwrap());
+/* Statements */
+
+const parsePrintStatement: Parser<PrintStatement> = Parser.string("kiír ").bind((_) => parseExpression.bindResult((exp) => make_print(exp)));
+
+const parseIfStatement: Parser<IfStatement> = Parser.doNotation<{ pred: Expression; tBlock: Block; fBlock: Block }>([
+  ["", Parser.string("ha ")],
+  ["pred", parseExpression],
+  ["", Parser.string(" akkor ")],
+  ["tBlock", Parser.of(() => parseBlock)],
+  ["", Parser.string(" különben ")],
+  ["fBlock", Parser.of(() => parseBlock)],
+  ["", Parser.string(" elágazás vége")],
+]).bindResult(({ pred, tBlock, fBlock }) => make_if(pred, tBlock, fBlock));
+
+const parseAssignmentStatement: Parser<AssignmentStatement> = Parser.doNotation<{ variable: Variable; value: Expression }>([
+  ["variable", parseVariable],
+  ["", Parser.string(" <- ")],
+  ["value", parseExpression],
+]).bindResult(({ variable, value }) => make_assignment(variable, value));
+
+const parseStatement: Parser<Statement> = parseAssignmentStatement.or(parsePrintStatement).or(parseIfStatement);
+const parseBlock = parseStatement.many();
+
+console.log(parseBlock.run("ha 1+1 akkor kiír (1, 2, 3) különben x <- 5   + 5 elágazás vége").unwrap());
--- js/src/pseudo_types.ts
+++ js/src/pseudo_types.ts
@@ -1,30 +1,73 @@
-/* Types */
+/* = Types = */
 
+/* Expressions */
 export type Atom = {
-  kind: "atom",
-  value: number | string | boolean | Array<number | string | boolean>
-}
+  kind: "atom";
+  value: number | string | boolean | Array<number | string | boolean>;
+};
 
 export type BinOp = {
-  kind: "binop",
+  kind: "binop";
   value: {
-    op: string,
-    exp1: Expression,
-    exp2: Expression
-  }
+    op: string;
+    exp1: Expression;
+    exp2: Expression;
+  };
+};
+
+export type Variable = {
+  kind: "variable";
+  value: string;
+};
+
+export type Value = Atom | Variable;
+
+export type Expression = Value | BinOp;
+
+/* Statements */
+export type PrintStatement = {
+  kind: "print";
+  value: Expression;
+};
+
+export type IfStatement = {
+  kind: "if";
+  pred: Expression;
+  truePath: Block;
+  falsePath: Block;
+};
+
+export type AssignmentStatement = {
+  kind: "assignment";
+  variable: Variable;
+  value: Expression;
+};
+
+export type Block = Array<Statement>;
+export type Statement = AssignmentStatement | IfStatement | PrintStatement | Expression;
+
+/* = Constructors = */
+
+export function make_atom(value: Atom["value"]): Atom {
+  return { kind: "atom", value };
 }
 
-export type Expression = Atom | BinOp;
-
-/* Constructors */
-
-export function make_atom(value: Atom['value']): Atom {
-  return { kind: "atom", value }
+export function make_binop(op: string, exp1: Expression, exp2: Expression): BinOp {
+  return { kind: "binop", value: { op, exp1, exp2 } };
 }
 
-export function make_binop(value: BinOp['value']): BinOp {
-  return {
-    kind: "binop",
-    value
-  }
+export function make_variable(value: Variable["value"]): Variable {
+  return { kind: "variable", value };
+}
+
+export function make_print(value: PrintStatement["value"]): PrintStatement {
+  return { kind: "print", value };
+}
+
+export function make_if(pred: Expression, tBlock: Block, fBlock: Block): IfStatement {
+  return { kind: "if", pred, truePath: tBlock, falsePath: fBlock };
+}
+
+export function make_assignment(variable: Variable, value: Expression): AssignmentStatement {
+  return { kind: "assignment", variable, value };
 }
