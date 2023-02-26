2f45e76d53783306f7e331e003d6136798868ae1
Nemin32 <pergerlori@gmail.com>
2023-02-26 13:33:59 +0100

    New token-based parser.

--- js/src/compiler/pseudo_parser.ts
+++ js/src/compiler/pseudo_parser.ts
@@ -1,4 +1,5 @@
-import { Parser } from "../parser/parser.ts";
+import { TokenType as TT, Tokenizer } from "../parser/tokenizer.ts";
+import { TokenToASTParser as P } from "../parser/token_parser.ts";
 import {
   ArithmeticBinOp,
   ArrayAssignment,
@@ -10,6 +11,7 @@ import {
   Block,
   Comparison,
   Debug,
+  DoWhile,
   Expression,
   For,
   FunctionCall,
@@ -25,315 +27,87 @@ import {
   While,
 } from "./pseudo_types.ts";
 
-// const OSPACES = Parser.char(" ").many();
-const SPACES = Parser.char(" ").many1();
+const matchToken = (type: TT) => P.sat((t) => t.type == type, "EOF!", "");
 
-// const ONL = Parser.char("\n").many();
-// const NL = Parser.char("\n").many1();
+const WS = matchToken(TT.WHITESPACE).maybe();
+const OWS = WS.maybe();
 
-const WS = Parser.or(Parser.char(" "), Parser.char("\n")).many1();
-const OWS = Parser.or(Parser.char(" "), Parser.char("\n")).many();
+/* = Groupings = */
 
-/*
-const tokenize = (words: string | string[]): Parser<string> => {
-  if (typeof words === "string") {
-    return OWS.right(Parser.string(words));
-  } else if (words.length === 1) {
-    return OWS.right(Parser.string(words[0]));
-  } else if (words.length === 0) {
-    throw new Error("Length must be more than one");
-  } else {
-    return OWS.right(
-      words.slice(1).reduce(
-        (p, str) => p.left(WS).bind((_) => Parser.string(str)),
-        Parser.string(words[0]),
-      ),
-    );
-  }
-};
-*/
-
-const parseFuncName: Parser<string> = Parser.upper.bind((l) =>
-  Parser.letter.many().bindResult((rest) => l + rest.join(""))
-);
-
-const parseType: Parser<string> = Parser.choice([
-  Parser.string("egész"),
-  Parser.string("szöveg"),
-]);
-
-/* Groupings */
-
-export const parseStatement: Parser<Statement> = OWS.right(
-  Parser.of(() =>
-    Parser.choice([
-      parseDebug,
-      parseReturn,
-      parseFuncCall,
-      parseFunctionDecl,
-      parseFor,
-      parseWhileStatement,
-      parseArrayAssignment,
-      parseArrayElementAssignment,
-      parseAssignmentStatement,
-      parsePrintStatement,
-      parseIfStatement,
-    ])
-  ),
-);
-
-export const parseExpression: Parser<Expression> = OWS.right(
-  Parser.of(() =>
-    Parser.choice([
-      parseArrayComprehension,
-      parseArrayIndex,
-      parseFuncCall,
-      parseNot,
-      parseComp,
-    ])
-  ),
-);
-
-export const parseBlock: Parser<Block> = OWS.right(parseStatement).sepBy(WS);
+const parseExpression: P<Expression> = P.of(() => parseNegate.or(parseNumber).or(parseBool).or(parseString));
+const parseStatement: P<Statement> = P.of(() => parseExpression.or(parsePrint).or(parseDoWhile).or(parseWhile).or(parseDebug));
+const parseBlock: P<Block> = OWS.right(parseStatement.sepBy(WS));
 
 /* = Expressions = */
 
-/* Comprehension */
-
-export const parseArrayComprehension = parseExpression
-  .sepBy(Parser.char(",").left(OWS))
-  .parens()
-  .bindResult((exps: Expression[]) => new ArrayComprehension(exps))
-  .expect("<expression list>");
-
 /* Atom */
+const parseNumber: P<Atom> = matchToken(TT.NUMBER).bindResult((token) => new Atom(Number(token.lexeme)));
+const parseString: P<Atom> = matchToken(TT.STRING).bindResult((token) => new Atom(token.lexeme.slice(1, token.lexeme.length - 1)));
+const parseBool: P<Atom> = matchToken(TT.BOOLEAN).bindResult((token) => new Atom(token.lexeme == "igaz"));
 
-export const parseNumber = Parser.digit.many1().bindResult((digits) =>
-  Number(digits.join(""))
-).expect(
-  "<number>",
-);
-
-export const parseString = Parser.sat((c) => c != '"')
-  .many()
-  .bracket(Parser.char('"'), Parser.char('"'))
-  .bindResult((chars) => chars.join(""))
-  .expect("<string>");
-
-export const parseBool = Parser.or(
-  Parser.string("igaz")
-    .or(Parser.string("Igaz"))
-    .bindResult((_) => true),
-  Parser.string("hamis")
-    .or(Parser.string("Hamis"))
-    .bindResult((_) => false),
-).expect("<bool>");
-
-export const parseAtom: Parser<Atom> = parseNumber
-  .or(parseString)
-  .or(parseBool)
-  .bindResult((v) => new Atom(v))
-  .expect("<atom>");
-
-/* Variable */
-
-const parseVarName: Parser<string> = Parser.letter.bind((l) =>
-  Parser.alphanumeric.many().bindResult((rest) => l + rest.join(""))
-);
-export const parseVariable: Parser<Variable> = parseVarName.bindResult((name) =>
-  new Variable(name)
-);
-
-/* Array Indexing */
-
-export const parseArrayIndex: Parser<ArrayIndex> = Parser.do()
-  .bind("variable", parseVariable)
-  .bind("index", parseExpression.bracket(Parser.char("["), Parser.char("]")))
-  .bindResult(({ variable, index }) => new ArrayIndex(variable, index));
-
-/* Value */
-
-export const parseValue = parseAtom.or(parseVariable);
-
-/* Binary Ops */
-
-export const parseCompOp: Parser<string> = Parser.choice([
-  Parser.string("=/="),
-  Parser.string("<="),
-  Parser.string(">="),
-  Parser.char("="),
-  Parser.char("<"),
-  Parser.char(">"),
-]).bracket(OWS, OWS);
-
-export const parseLogicOp: Parser<string> = Parser.or(
-  Parser.string("&&"),
-  Parser.string("||"),
-).bracket(OWS, OWS);
-export const parseMulOp: Parser<string> = Parser.char("*").or(Parser.char("/"))
-  .or(Parser.string("mod")).bracket(OWS, OWS);
-export const parseAddOp: Parser<string> = Parser.char("+").or(Parser.char("-"))
-  .bracket(OWS, OWS);
-
-const parseFactor: Parser<Expression> = parseValue.or(
-  Parser.of(() => parseLogic).parens(),
-);
-
-const parseArithmAdd: Parser<Expression> = parseFactor
-  .bindChain(parseAddOp, ({ f, a, b }) => new ArithmeticBinOp(f, a, b));
-
-const parseArithmMul: Parser<Expression> = parseArithmAdd
-  .bindChain(parseMulOp, ({ f, a, b }) => new ArithmeticBinOp(f, a, b));
-
-const parseComp: Parser<Expression> = parseArithmMul
-  .bindChain(parseCompOp, ({ f, a, b }) => new Comparison(f, a, b));
-
-const parseLogic: Parser<Expression> = parseComp
-  .bindChain(parseLogicOp, ({ f, a, b }) => new LogicBinOp(f, a, b));
-
-/* Not */
-
-export const parseNot: Parser<Not> = Parser.char("~")
+/* Negation */
+const parseNegate = matchToken(TT.NEGAL)
   .right(parseExpression)
-  .bindResult((e) => new Not(e));
-
-/* Function Call */
-
-const parseExpressionList = Parser.or(
-  OWS.parens().bindResult((_) => []),
-  parseExpression.sepBy(Parser.char(",").left(OWS))
-    .parens(),
-);
-
-export const parseFuncCall: Parser<FunctionCall> = Parser.do()
-  .bind("name", parseFuncName.left(OWS))
-  .bind("params", parseExpressionList)
-  .bindResult(({ name, params }) => new FunctionCall(name, params));
+  .bindResult((exp) => new Not(exp));
 
 /* = Statements = */
 
-/* Debug */
+const parseFuncName = matchToken(TT.FUNCNAME).bindResult((t) => t.lexeme);
 
-export const parseDebug: Parser<Debug> = Parser.string("debug").bindResult(
-  (_) => new Debug(),
-);
+const parseSymbol = matchToken(TT.SYMBOL).bindResult((t) => t.lexeme);
+const parseType = matchToken(TT.TYPE).bindResult((t) => t.lexeme);
+
+const parseParameter = P.do()
+  .bind("ref", matchToken(TT.CIMSZERINT).maybe())
+  .bind("name", parseSymbol)
+  .ignore(matchToken(TT.COLON))
+  .bind("type", parseType)
+  .ignore(matchToken(TT.TOMB).maybe())
+  .bindResult(({ ref, name, _type }) => new Parameter(name, ref != null));
+
+const parseParamList = parseParameter.sepBy(matchToken(TT.COMMA)).bracket(matchToken(TT.OPAREN), matchToken(TT.CPAREN));
+
+const parseFuncDecl = P.do()
+  .ignore(matchToken(TT.FUGGVENY))
+  .bind("name", parseFuncName)
+  .bind("params", parseParamList)
+  .bind("body", parseBlock)
+  .ignore(matchToken(TT.FUGGVENY))
+  .ignore(matchToken(TT.VEGE))
+  .bindResult(({ name, params, body }) => new FunctionDeclaration(name, params, body));
 
 /* Print */
+const parsePrint = matchToken(TT.KIIR)
+  .right(WS)
+  .right(parseExpression)
+  .bindResult((exp) => new Print(exp));
 
-export const parsePrintStatement: Parser<Print> = Parser.string("kiír ")
-  .bind((_) => parseExpression.bindResult((exp: Expression) => new Print(exp)));
+/* Debug */
+const parseDebug = matchToken(TT.DEBUG).bindResult((_) => new Debug());
 
-/* If */
-
-const parseIfHead: Parser<{ pred: Expression; body: Block }> = Parser.do()
-  .ignore(Parser.string("ha").left(WS))
+/* While */
+const parseWhile = P.do()
+  .ignore(matchToken(TT.CIKLUS))
+  .ignore(matchToken(TT.AMIG))
   .bind("pred", parseExpression)
-  .ignore(Parser.string("akkor").bracket(WS, WS))
   .bind("body", parseBlock)
-  .toParser();
-
-const parseElse: Parser<Block> = Parser.string("különben").bracket(WS, WS)
-  .right(parseBlock);
-
-const parseElseIfBranches: Parser<Array<{ pred: Expression; body: Block }>> =
-  Parser.do()
-    .ignore(Parser.string("különben ha").bracket(WS, WS))
-    .bind("pred", parseExpression)
-    .ignore(Parser.string("akkor").bracket(WS, WS))
-    .bind("body", parseBlock)
-    .toParser().many1();
-
-// if ...
-const parseIf: Parser<If> = parseIfHead.left(
-  OWS.right(Parser.string("elágazás vége")),
-).bindResult((head) => new If(head, [], null));
-
-// if ... else ...
-const parseIfElse: Parser<If> = Parser.do()
-  .bind("head", parseIfHead)
-  .bind("elseBranch", parseElse)
-  .ignore(OWS.right(Parser.string("elágazás vége")))
-  .bindResult(({ head, elseBranch }) => new If(head, [], elseBranch));
-
-// if ... (else if...)* else ...
-const parseElseIfStatement: Parser<If> = Parser.do()
-  .bind("head", parseIfHead)
-  .bind("elIf", parseElseIfBranches)
-  .bind("elseBranch", parseElse)
-  .ignore(OWS.right(Parser.string("elágazás vége")))
-  .bindResult(({ head, elIf, elseBranch }) => new If(head, elIf, elseBranch));
-
-const parseIfStatement = parseElseIfStatement.or(parseIfElse).or(parseIf);
-
-/* For */
-
-export const parseFor: Parser<For> = Parser.do()
-  .ignore(Parser.string("ciklus").left(WS))
-  .bind("variable", parseVariable)
-  .ignore(Parser.string("<-").bracket(OWS, OWS))
-  .bind("from", parseExpression.left(Parser.or(Parser.string("-tól"), Parser.string("-től")).left(WS)))
-  .bind("to", parseExpression.left(Parser.string("-ig")))
-  .bind("body", parseBlock)
-  .ignore(Parser.string("ciklus vége"))
-  .bindResult(({variable, from, to, body}) => new For(variable, from, to, body))
-
-/* Arrays */
-
-const parseArrayAssignment: Parser<ArrayAssignment> = Parser.do()
-  .bind("variable", parseVariable)
-  .ignore(Parser.string("<-").bracket(OWS, OWS))
-  .ignore(Parser.string("Létrehoz"))
-  .bind("type", parseType.bracket(Parser.char("["), Parser.char("]")))
-  .bind("length", parseExpression.parens())
-  .bindResult(({ variable, type, length }) =>
-    new ArrayAssignment(variable, type, length)
-  );
-
-export const parseAssignmentStatement: Parser<Assignment> = Parser.do()
-  .bind("variable", parseVariable)
-  .ignore(Parser.string("<-").bracket(OWS, OWS))
-  .bind("value", parseExpression)
-  .bindResult(({ variable, value }) => new Assignment(variable, value));
-
-export const parseWhileStatement: Parser<While> = Parser.do()
-  .ignore(Parser.string("ciklus amíg").right(WS))
-  .bind("pred", parseExpression.left(WS))
-  .bind("body", parseBlock.left(WS))
-  .ignore(Parser.string("ciklus vége"))
+  .ignore(matchToken(TT.CIKLUS))
+  .ignore(matchToken(TT.VEGE))
   .bindResult(({ pred, body }) => new While(pred, body));
 
-const parseParameter: Parser<Parameter> = Parser.do()
-  .bind("isRef", Parser.string("címszerint").left(OWS).maybe())
-  .bind("varName", parseVarName)
-  .ignore(Parser.char(":").bracket(OWS, OWS))
-  .bind("type", parseType)
-  .bindResult(({ varName, isRef }) => new Parameter(varName, isRef != null));
+/* Do While */
+const parseDoWhile = P.do()
+  .ignore(matchToken(TT.CIKLUS))
+  .bind("body", parseBlock)
+  .ignore(matchToken(TT.AMIG))
+  .bind("pred", parseExpression)
+  .bindResult(({ pred, body }) => new DoWhile(pred, body));
 
-const parseFuncList = Parser.or(
-  OWS.parens().bindResult((_) => []),
-  parseParameter.sepBy(Parser.char(",").left(OWS)).parens(),
-);
+function run(input: string) {
+  const tokenizer = new Tokenizer(input);
+  const tokens = tokenizer.parse().filter((t) => t.type != TT.WHITESPACE);
 
-export const parseFunctionDecl: Parser<FunctionDeclaration> = Parser.do()
-  .ignore(Parser.string("függvény").left(WS))
-  .bind("funcName", parseFuncName.left(OWS))
-  .bind("params", parseFuncList.left(WS))
-  .bind("body", parseBlock.left(WS))
-  .ignore(Parser.string("függvény vége"))
-  .bindResult(({ funcName, params, body }) =>
-    new FunctionDeclaration(funcName, params, body)
-  );
+  return parseBlock.run(tokens).filter((c) => c.kind == "capture")?.[0]?.value;
+}
 
-export const parseReturn: Parser<Return> = Parser.string("vissza")
-  .left(SPACES)
-  .right(parseExpression)
-  .bindResult((e) => new Return(e));
-
-export const parseArrayElementAssignment: Parser<ArrayElementAssignment> =
-  Parser.do()
-    .bind("arrayIndex", parseArrayIndex)
-    .ignore(Parser.string("<-").bracket(OWS, OWS))
-    .bind("expression", parseExpression)
-    .bindResult(({ arrayIndex, expression }) =>
-      new ArrayElementAssignment(arrayIndex, expression)
-    );
+console.log(run("ciklus kiír 5 amíg 5"));
---
+++ js/src/parser/token_parser.ts
@@ -0,0 +1,167 @@
+import { PseudoToken } from "./tokenizer.ts";
+
+type Error = string;
+type Token = PseudoToken;
+
+type InputWrap<T> = { tokens: T[]; index: number };
+type Capture<T, R> = { kind: "capture"; next: InputWrap<T>; value: R };
+type CError<T, E> = { kind: "error"; where: InputWrap<T>; value: E };
+
+type ParseInput = InputWrap<Token>;
+type ParseResult<T> = (Capture<Token, T> | CError<Token, Error>)[];
+
+export class TokenToASTParser<T> {
+  constructor(private exec: (input: ParseInput) => ParseResult<T>) {}
+
+  wrap(input: Token[]): ParseInput {
+    return {
+      tokens: input,
+      index: 0,
+    };
+  }
+
+  run(input: Token[]) {
+    return this.exec(this.wrap(input));
+  }
+
+  static result = <Q>(value: Q): TokenToASTParser<Q> =>
+    new TokenToASTParser(
+      (input: ParseInput): ParseResult<Q> => [
+        {
+          kind: "capture",
+          next: input,
+          value,
+        },
+      ]
+    );
+
+  static zero = (error: Error): TokenToASTParser<never> =>
+    new TokenToASTParser(
+      (inp: ParseInput): ParseResult<never> => [
+        {
+          kind: "error",
+          where: inp,
+          value: error,
+        },
+      ]
+    );
+
+  static item = (error: Error): TokenToASTParser<Token> =>
+    new TokenToASTParser((inp: ParseInput): ParseResult<Token> => {
+      if (inp.index >= inp.tokens.length) {
+        return [{ kind: "error", where: inp, value: error }];
+      }
+
+      return [
+        {
+          kind: "capture",
+          next: { tokens: inp.tokens, index: inp.index + 1 },
+          value: inp.tokens[inp.index],
+        },
+      ];
+    });
+
+  bind<Q>(func: (value: T, prevInput: ParseInput) => TokenToASTParser<Q>): TokenToASTParser<Q> {
+    return new TokenToASTParser<Q>((inp: ParseInput) => this.exec(inp).flatMap((fR) => (fR.kind == "error" ? fR : func(fR.value, fR.next).exec(fR.next))));
+  }
+
+  static sat = (predicate: (value: Token) => boolean, eofError: Error, noSatError: Error) => {
+    return TokenToASTParser.item(eofError).bind((value) => (predicate(value) ? TokenToASTParser.result(value) : TokenToASTParser.zero(noSatError)));
+  };
+
+  or = <Q>(other: TokenToASTParser<Q>): TokenToASTParser<Q | T> =>
+    new TokenToASTParser<Q | T>((inp) => {
+      const firstRun = this.exec(inp) as ParseResult<Q | T>;
+
+      return firstRun.flatMap((run) => {
+        if (run.kind == "capture") {
+          return run;
+        } else {
+          return other.exec(inp) as ParseResult<Q | T>;
+        }
+      });
+    });
+
+  sepBy = <S>(separator: TokenToASTParser<S>): TokenToASTParser<T[]> => {
+    const valueBind = (_: S) => this.bind(TokenToASTParser.result);
+    const resultConcat = (x: T, xs: T[]): TokenToASTParser<T[]> => TokenToASTParser.result([x].concat(xs));
+    const sepBind = separator.bind(valueBind);
+
+    return this.bind((x) => sepBind.many().bind((xs) => resultConcat(x, xs)));
+  };
+
+  bindResult<W>(f: (val: T, inp: ParseInput) => W): TokenToASTParser<W> {
+    return this.bind((val, inp) => TokenToASTParser.result(f(val, inp)));
+  }
+
+  left = <Q>(other: TokenToASTParser<Q>): TokenToASTParser<T> => this.bind((v) => other.bindResult((_) => v));
+  right = <Q>(other: TokenToASTParser<Q>): TokenToASTParser<Q> => this.bind((_) => other.bindResult((v) => v));
+  bracket = <L, R>(leftB: TokenToASTParser<L>, rightB: TokenToASTParser<R>): TokenToASTParser<T> => leftB.right(this).left(rightB);
+
+  plus = <Q>(other: TokenToASTParser<Q>): TokenToASTParser<T | Q> =>
+    new TokenToASTParser<T | Q>((inp) => (this as TokenToASTParser<T | Q>).exec(inp).concat(other.exec(inp)));
+
+  many1 = (): TokenToASTParser<T[]> => this.bind((x) => this.many().bindResult((xs) => [x].concat(xs)));
+  many = (): TokenToASTParser<T[]> => this.many1().plus(TokenToASTParser.result<T[]>([]));
+  maybe = (): TokenToASTParser<T | null> => this.or(TokenToASTParser.result(null));
+
+  static exact = <T>(value: T) => TokenToASTParser.sat((elem) => elem == value, "EOF!", "Expected " + value);
+  static of = <T>(p: () => TokenToASTParser<T>) => new TokenToASTParser<T>((inp) => p().exec(inp));
+
+  static do() {
+    return new BaseDo([], []);
+  }
+}
+
+class BaseDo<B extends Record<symbol | number | string, never>> {
+  // deno-lint-ignore no-explicit-any
+  constructor(public parsers: TokenToASTParser<any>[], public names: string[]) {}
+
+  // deno-lint-ignore no-explicit-any
+  bind<BindName extends string, BindType extends any>(name: BindName, parser: TokenToASTParser<BindType>): BaseDo<B & Record<BindName, BindType>> {
+    return new BaseDo(this.parsers.concat([parser]), this.names.concat([name]));
+  }
+
+  // deno-lint-ignore no-explicit-any
+  ignore(parser: TokenToASTParser<any>): BaseDo<B> {
+    return new BaseDo(this.parsers.concat([parser]), this.names.concat([""]));
+  }
+
+  bindResult<T>(f: (val: B) => T): TokenToASTParser<T> {
+    // deno-lint-ignore no-explicit-any
+    const zipped: Array<[string, TokenToASTParser<any>]> = this.parsers.map((p, idx) => [this.names[idx], p]);
+    return BaseDo.finalize<B>(zipped).bindResult(f);
+  }
+
+  toBaseParser(): TokenToASTParser<B> {
+    return this.bindResult<B>((val) => val);
+  }
+
+  /** @internal */
+  // deno-lint-ignore no-explicit-any
+  private static finalize<Obj>(parsers: Array<[string, TokenToASTParser<any>]>): TokenToASTParser<Obj> {
+    const descend = (
+      // deno-lint-ignore no-explicit-any
+      list: Array<[string, TokenToASTParser<any>]>,
+      // deno-lint-ignore no-explicit-any
+      obj: Record<string, any>
+      // deno-lint-ignore no-explicit-any
+    ): TokenToASTParser<any> => {
+      if (list.length == 0) {
+        return TokenToASTParser.result(obj);
+      } else {
+        const [varName, parser] = list[0];
+
+        return parser.bind((value) => {
+          if (varName != "") {
+            obj[varName] = value;
+          }
+
+          return descend(list.slice(1), obj);
+        });
+      }
+    };
+
+    return descend(parsers, {});
+  }
+}
