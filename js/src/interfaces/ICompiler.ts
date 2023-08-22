da95fc4e9637f70bc16b9af2b1169c227feb039a
Nemin32 <pergerlori@gmail.com>
2023-08-22 14:43:01 +0200

    Interface enums.

--- js/src/interfaces/ICompiler.ts
+++ js/src/interfaces/ICompiler.ts
@@ -1,11 +1,42 @@
 import { AtomValue, IAST } from "./IParser";
 
 // IAST -> IByteCode
-enum OpCode {}
+enum OpCode {
+  ADDRESS = 0,
+  ARRADDR = 1,
+
+  CALL = 2,
+  RETURN = 3,
+
+  VALARR = 4,
+  PUSH = 5,
+  BINOP = 6,
+  NOT = 7,
+  PRINT = 8,
+  VOID = 9,
+
+  GETARR = 10,
+  GETVAR = 11,
+
+  SETARR = 12,
+  SETVAR = 13,
+
+  LABEL = 14,
+  FJMP = 15,
+  JMP = 16,
+
+  ESCOPE = 17,
+  LSCOPE = 18,
+
+  MKARR = 19,
+  MKREF = 20,
+
+  DEBUG = 21,
+}
 
 export interface IByteCode {
   opCode: OpCode;
-  payload?: AtomValue
+  payload?: AtomValue;
   ast: IAST;
 }
 
--- js/src/interfaces/ITokenizer.ts
+++ js/src/interfaces/ITokenizer.ts
@@ -1,5 +1,48 @@
 // string -> IToken[]
-enum TokenType {}
+enum TokenType {
+  /* Keywords */
+  AKKOR = 0,
+  AMIG = 1,
+  CIKLUS = 2,
+  CIMSZERINT = 3,
+  DEBUG = 4,
+  ELAGAZAS = 5,
+  FUGGVENY = 6,
+  HA = 7,
+  KIIR = 8,
+  KULONBEN = 9,
+  LETREHOZ = 10,
+  TOMB = 11,
+  VEGE = 12,
+  VISSZA = 13,
+
+  /* Misc. */
+  NEGAL = 14,
+  OPAREN = 15,
+  CPAREN = 16,
+  OBRACKET = 17,
+  CBRACKET = 18,
+  COLON = 19,
+  COMMA = 20,
+  FORSTART = 21,
+  FOREND = 22,
+  NYIL = 23,
+  REFERENCE = 24,
+
+  /* Special */
+  NUMBER = 25,
+  BOOLEAN = 26,
+  STRING = 27,
+  SYMBOL = 28,
+  FUNCNAME = 29,
+  ARITHMOP = 30,
+  COMPOP = 31,
+  LOGICOP = 32,
+  WHITESPACE = 33,
+  TYPE = 34,
+
+  ERROR = 35,
+}
 
 export interface IToken {
   readonly lexeme: string;
